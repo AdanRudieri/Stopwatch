307075a9c8f3f0711ef387f2f017107cdcf97c05
AdanRudieri <adanrudieri@gmail.com>
2023-06-08 14:50:30 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Stopwhatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("--- M E N U . S T O P W H A T C H ---");
+            Console.WriteLine("S = Second => 10s = 10 seconds");
+            Console.WriteLine("M = Minute => 10m = 10 minutes");
+            Console.WriteLine("0 = Exit");
+            Console.WriteLine("How long do you want to count?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            int multiplier = 1;
+            if (type == 'm')
+                multiplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready....");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set....");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+            Console.WriteLine();
+
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("stopwatch finished!");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
+
